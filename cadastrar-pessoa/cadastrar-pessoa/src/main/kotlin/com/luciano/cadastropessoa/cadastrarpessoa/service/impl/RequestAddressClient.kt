4fd13780f201ee9dab19bd32701c1d30e133cadc
Luciano <lualgoritmo@outlook.com>
2024-02-22 16:19:57 -0300

    Consumir um microservice

--- cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/service/impl/RequestAddressClient.kt
+++ cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/service/impl/RequestAddressClient.kt
@@ -2,11 +2,12 @@ package com.luciano.cadastropessoa.cadastrarpessoa.service.impl
 
 import com.luciano.cadastropessoa.cadastrarpessoa.controller.dto.ResponseCEPDTO
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name = "solicitar-cep", url = "viacep.com.br")
+@FeignClient(name = "solicitar-cep", url = "https://viacep.com.br")
 interface RequestAddressClient {
-    @GetMapping("/ws/{cep}/json/")
+    @GetMapping("/ws/{cep}/json/", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun getAddressByCep(@PathVariable cep: String): ResponseCEPDTO
 }
