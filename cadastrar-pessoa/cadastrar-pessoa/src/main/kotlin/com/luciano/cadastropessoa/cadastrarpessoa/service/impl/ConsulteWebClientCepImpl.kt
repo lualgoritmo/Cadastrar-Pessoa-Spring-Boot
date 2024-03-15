57c1fff830992b8e4ed5b1e5db03a365922eeac0
Luciano <lualgoritmo@outlook.com>
2024-03-15 17:36:28 -0300

    Refatorar

--- cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/service/impl/ConsulteWebClientCepImpl.kt
+++ cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/service/impl/ConsulteWebClientCepImpl.kt
@@ -2,22 +2,28 @@ package com.luciano.cadastropessoa.cadastrarpessoa.service.impl
 
 import com.luciano.cadastropessoa.cadastrarpessoa.controller.dto.ResponseCEPDTO
 import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
 
+
 @Service
-class ConsulteWebClientCepImpl(private val clientWeb: WebClient) {
+class ConsulteWebClientCepImpl(
+        private val clientWeb: WebClient,
+        @Value("\${via_cep_uri}")
+        val viaCepURI: String
+) {
     suspend fun consulteViaCep(cep: String): ResponseCEPDTO {
-        val uri = UriComponentsBuilder.fromUriString("https://viacep.com.br/ws/{cep}/json/")
+        val uri = UriComponentsBuilder.fromUriString(viaCepURI)
                 .buildAndExpand(cep)
                 .toUri()
 
         return clientWeb.get()
-                    .uri(uri)
-                    .retrieve()
-                    .bodyToMono(ResponseCEPDTO::class.java)
-                    .awaitFirstOrNull() ?: throw RuntimeException("DEU ERRO AQUI")
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(ResponseCEPDTO::class.java)
+                .awaitFirstOrNull() ?: throw RuntimeException("DEU ERRO AQUI")
 
     }
 }
