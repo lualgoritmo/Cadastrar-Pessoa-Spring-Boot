c48f7c6c2733599eedb922970d4e74f8fcb013f9
Luciano <lualgoritmo@outlook.com>
2024-03-29 22:40:06 -0300

    Kafka container

--- cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/CadastrarPessoaApplication.kt
+++ cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/CadastrarPessoaApplication.kt
@@ -3,9 +3,7 @@ package com.luciano.cadastropessoa.cadastrarpessoa
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.kafka.annotation.EnableKafka
 
-@EnableKafka
 @EnableFeignClients
 @SpringBootApplication
 class CadastrarPessoaApplication
--- cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/controller/KafkaTestController.kt
+++ cadastrar-pessoa/cadastrar-pessoa/src/main/kotlin/com/luciano/cadastropessoa/cadastrarpessoa/controller/KafkaTestController.kt
@@ -1,21 +1,18 @@
-package com.luciano.cadastropessoa.cadastrarpessoa.controller
-
-import com.luciano.cadastropessoa.cadastrarpessoa.service.impl.AuthorTestProducer
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-@RequestMapping("/kafka")
-class KafkaTestController(private val service: AuthorTestProducer) {
-
-    @GetMapping("/hello/name")
-    @ResponseStatus(HttpStatus.OK)
-    fun hello(@PathVariable name:String) {
-        service.sendMessage("Funfando tudo: $name")
-    }
-
-}
+//package com.luciano.cadastropessoa.cadastrarpessoa.controller
+//
+//import com.luciano.cadastropessoa.cadastrarpessoa.service.impl.AuthorTestProducer
+//import org.springframework.http.HttpStatus
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.PathVariable
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.ResponseStatus
+//import org.springframework.web.bind.annotation.RestController
+//
+//@RestController
+//@RequestMapping("/kafka")
+//class KafkaTestController(private val service: AuthorTestProducer) {
+//    @GetMapping("/hello/name")
+//    @ResponseStatus(HttpStatus.OK)
+//    fun hello(@PathVariable name:String) = service.sendMessage("Funfando tudo: $name")
+//
+//}
